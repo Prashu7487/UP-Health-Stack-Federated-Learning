ce126e2f8504a4e5cafe8d1e56ca91b09790899e
Prashu7487 <pm7487912@gmail.com>
2024-05-20 08:43:28 +0530

    "Improved global_parameters to accommodate generalized weights"

--- Server/Server.py
+++ Server/Server.py
@@ -4,10 +4,8 @@ import aiohttp
 import asyncio
 
 
-
-
 class Server:
-    def __init__(self,globals_parameters, max_round):
+    def __init__(self, globals_parameters, max_round):
         self.globals_parameters = globals_parameters
         self.client_parameters = {}
         self.curr_round = 1
@@ -30,7 +28,7 @@ class Server:
                     # print("Check : ",vectors)
                     aggregated_sums[key] = np.zeros_like(vectors)
                 # Sum the vectors for the current key
-                #print(key,aggregated_sums[key],type(vectors))
+                # print(key,aggregated_sums[key],type(vectors))
                 aggregated_sums[key] += np.array(vectors)
 
         # Divide the aggregated sums by the number of clients and convert to list
@@ -38,17 +36,13 @@ class Server:
             aggregated_sums[key] /= num_clients
             aggregated_sums[key] = aggregated_sums[key].tolist()
 
-        print("Aggregate Weights after FedAvg: ",aggregated_sums)
+        print("Aggregate Weights after FedAvg: ", aggregated_sums)
 
         self.globals_parameters = aggregated_sums
         self.clear_client_parameters()
 
 
-
-
-
-
-async def send_request(session, url, data, client_id,server):
+async def send_request(session, url, data, client_id, server):
     async with session.post(url, json=data) as response:
         print(f"Response from client {client_id}: {response.status}")
         response_data = await response.json()  # Return the JSON response
@@ -57,50 +51,48 @@ async def send_request(session, url, data, client_id,server):
         server.client_parameters[client_id] = client_parameters
 
 
-
-
-async def send_updated_parameters_to_clients(server,client_iter):
-    updated_parameters = {"parameters" : server.globals_parameters,"round_num":server.curr_round,
-                        "client_iter" : client_iter}
+async def send_updated_parameters_to_clients(server, client_iter):
+    updated_parameters = {"parameters": server.globals_parameters, "round_num": server.curr_round,
+                          "client_iter": client_iter}
     async with aiohttp.ClientSession() as session:
         tasks = []
         for client_id in range(1, num_clients + 1):
             client_url = f"http://localhost:500{client_id}/receive_parameters_and_run_model"
-            task = asyncio.create_task(send_request(session, client_url, updated_parameters, client_id,server))
+            task = asyncio.create_task(send_request(session, client_url, updated_parameters, client_id, server))
             tasks.append(task)
         await asyncio.gather(*tasks)  # Gather responses
 
 
-
-
 # Modify the __main__ block accordingly to use the responses
 if __name__ == '__main__':
-    
-    
     # Define the size of arrays
-    m_size = 22
-    c_size = 1
+    # m_size = 22
+    # c_size = 1
+
+    weights_shape = (1, 22)
+    bias_shape = (1, 1)
+
     num_clients = 2  # Assuming there are 5 clients
     max_round = 3
     client_iter = 2
 
-
     # Initialize the global parameters dictionary
-    global_parameters = {"m": [0 for i in range(m_size)], "c": [0 for i in range(c_size)]}
+    # global_parameters = {"m": [0 for i in range(m_size)], "c": [0 for i in range(c_size)]}
+    global_parameters = {"weights": np.zeros(weights_shape).tolist(),
+                         "bias": np.zeros(bias_shape).tolist()}
 
-
-    server = Server(global_parameters,max_round)
+    server = Server(global_parameters, max_round)
 
     start_learning = input("Press 'Y' to start Federated Learning: ")
     if start_learning.upper() == 'Y':
-        for i in range(1,max_round+1):
+        for i in range(1, max_round+1):
             print("-"*50)
             print(f"Round {i}")
             print("-"*50)
             loop = asyncio.get_event_loop()
-            responses = loop.run_until_complete(send_updated_parameters_to_clients(server,client_iter))
+            responses = loop.run_until_complete(send_updated_parameters_to_clients(server, client_iter))
 
-            print("\nClient Parameters : ",server.client_parameters,'\n')
+            print("\nClient Parameters : ", server.client_parameters, '\n')
 
             server.curr_round += 1
             # Aggregate 
